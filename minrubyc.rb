c2e2c2339673b5c9695f2d9d8ce1f172a84fc845
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2023-07-29 20:52:23 +0900

    四則演算を実装

--- minrubyc.rb
+++ minrubyc.rb
@@ -8,21 +8,41 @@ require "minruby"
 def gen(tree)
   if tree[0] == "lit"
     puts "\tmov rax, #{tree[1]}"
-  elsif tree[0] == "+"
-    # R12を退避
+  elsif %w(+ - * /).include?(tree[0])
+    # R12とR13をスタックへ退避
     puts "\tpush r12"
     puts "\tpush r13"
 
-    # 左辺を計算してR12に結果を入れる
+    # 左辺を計算してR12へ結果を格納
     gen(tree[1])
     puts "\tmov r12, rax"
 
-    # 右辺を計算してR13に結果を入れる
+    # 右辺を計算してR13へ結果を格納
     gen(tree[2])
     puts "\tmov r13, rax"
 
-    # R12 + R13の結果をRAXに入れる
-    puts "\tadd rax, r12"
+    # 演算結果をRAXへ格納
+    case tree[0]
+    when "+"
+      puts "\tadd r12, r13"
+      puts "\tmov rax, r12"
+    when "-"
+      puts "\tsub r12, r13"
+      puts "\tmov rax, r12"
+    when "*"
+      puts "\timul r12, r13"
+      puts "\tmov rax, r12"
+    when "/"
+      puts "\tmov rax, r12"
+      puts "\tcqo"
+      puts "\tidiv r13"
+    else
+      raise "invalid operator: #{tree[0]}"
+    end
+
+    # R12とR13をスタックから復元
+    puts "\tpop r13"
+    puts "\tpop r12"
   else
     raise "invalid AST: #{tree}"
   end
--- test.sh
+++ test.sh
@@ -61,7 +61,10 @@ assert() {
 # assert 1 'p(1 >= 1)'
 # assert 0 'p(0 >= 1)'
 
-assert 30 '10+20'
+assert 33 '99 / 3'
+assert 200 '10 * 20'
+assert 40 '30 + 20 - 10'
+assert 60 '10 + 20 + 30'
 assert 4649 '4649'
 
 echo OK
